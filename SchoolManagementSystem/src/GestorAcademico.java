210f67c18878191e6bd82417fed92652d2c2765a
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-16 18:36:12 -0600

    ArrayList creado para Estudiantes

--- SchoolManagementSystem/src/GestorAcademico.java
+++ SchoolManagementSystem/src/GestorAcademico.java
@@ -1,2 +1,17 @@
+import java.util.ArrayList;
+
 public class GestorAcademico {
+    private ArrayList<Estudiante> estudiantes;
+
+    public GestorAcademico() {
+        estudiantes = new ArrayList<>();
+    }
+
+    public void agregarEstudiante(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+    public ArrayList<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
 }
