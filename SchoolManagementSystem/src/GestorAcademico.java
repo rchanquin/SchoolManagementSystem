815943a82dbe84bd6ef70c0d7af1326c1e5611c5
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-16 18:39:39 -0600

    ArrayList creado para Cursos

--- SchoolManagementSystem/src/GestorAcademico.java
+++ SchoolManagementSystem/src/GestorAcademico.java
@@ -2,9 +2,11 @@ import java.util.ArrayList;
 
 public class GestorAcademico {
     private ArrayList<Estudiante> estudiantes;
+    private ArrayList<Curso> cursos;
 
     public GestorAcademico() {
         estudiantes = new ArrayList<>();
+        cursos = new ArrayList<>();
     }
 
     public void agregarEstudiante(Estudiante estudiante) {
@@ -14,4 +16,15 @@ public class GestorAcademico {
     public ArrayList<Estudiante> getEstudiantes() {
         return estudiantes;
     }
+
+    //* Cursos
+    public void agregarCurso(Curso curso) {
+        cursos.add(curso);
+    }
+
+    public ArrayList<Curso> getCursos() {
+        return cursos;
+    }
+
+
 }
