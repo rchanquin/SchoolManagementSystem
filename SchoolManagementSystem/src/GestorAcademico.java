424448dc282932770c5488699e61d33c4edc7b59
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-16 18:45:45 -0600

    Creación de HashMap para inscribir estudiantes a cursos

--- SchoolManagementSystem/src/GestorAcademico.java
+++ SchoolManagementSystem/src/GestorAcademico.java
@@ -1,12 +1,15 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class GestorAcademico {
     private ArrayList<Estudiante> estudiantes;
     private ArrayList<Curso> cursos;
+    private HashMap<Curso, ArrayList<Estudiante>> inscripciones;
 
     public GestorAcademico() {
         estudiantes = new ArrayList<>();
         cursos = new ArrayList<>();
+        inscripciones = new HashMap<>();
     }
 
     public void agregarEstudiante(Estudiante estudiante) {
@@ -25,6 +28,29 @@ public class GestorAcademico {
     public ArrayList<Curso> getCursos() {
         return cursos;
     }
+
+    //* Estudiantes inscritos
+
+    public void agregarEstudiates(Estudiante estudiante) {
+        estudiantes.add(estudiante);
+    }
+
+    public void agregarCursos(Curso curso) {
+        cursos.add(curso);
+    }
+
+    public void inscribirEstudiante(Estudiante estudiante, Curso curso) {
+        if (inscripciones.containsKey(curso)) {
+            inscripciones.get(curso).add(estudiante);
+        }
+    }
+
+    public ArrayList<Estudiante> getEstudiantesInscritos(Curso curso) {
+        return inscripciones.get(curso);
+    }
+
+    
+
 
 
 }
