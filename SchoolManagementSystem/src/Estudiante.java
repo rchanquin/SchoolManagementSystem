ba57c2bb2c4101e8699fbb9d5f963539a9020d5b
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-16 18:24:45 -0600

    modelo estudiante creado

--- SchoolManagementSystem/src/Estudiante.java
+++ SchoolManagementSystem/src/Estudiante.java
@@ -1,2 +1,54 @@
 public class Estudiante {
+    private String id;
+    private String nombre;
+    private String apellido;
+    private String fechaDeNacimiento;
+    private Estado estado;
+
+
+    public enum Estado {
+       matriculado,
+        inactivo,
+        graduado
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public Estado getEstado() {
+        return estado;
+    }
+
+    public void setEstado(Estado estado) {
+        this.estado = estado;
+    }
 }
