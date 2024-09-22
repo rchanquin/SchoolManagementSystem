121ee36cd7226b71d7a276f023b15fd3f23f2ff1
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-22 15:52:09 -0600

    Creación de modelo Persona y utilizar los atributos de Estudiante

---
+++ SchoolManagementSystem/src/model/Persona.java
@@ -0,0 +1,51 @@
+package model;
+
+public class Persona {
+    protected int id;
+    protected String nombre;
+    protected String apellido;
+    protected String fechaDeNacimiento;
+
+    public Persona() {
+
+    }
+
+    public Persona(int id, String nombre, String apellido, String fechaDeNacimiento) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellido() {
+        return apellido;
+    }
+
+    public void setApellido(String apellido) {
+        this.apellido = apellido;
+    }
+
+    public String getFechaDeNacimiento() {
+        return fechaDeNacimiento;
+    }
+
+    public void setFechaDeNacimiento(String fechaDeNacimiento) {
+        this.fechaDeNacimiento = fechaDeNacimiento;
+    }
+}
