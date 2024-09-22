1c92c731df1e5d2ca70baa793c110c7f0d8fbdc9
Rodrigo Mac <macrodrigo10@gmail.com>
2024-09-22 15:41:47 -0600

    Implementación de interfase ServiciosAcademicos a GestorAcademico

--- SchoolManagementSystem/src/model/GestorAcademico.java
+++ SchoolManagementSystem/src/model/GestorAcademico.java
@@ -1,9 +1,11 @@
 package model;
 
+import Interface.ServiciosAcademicos;
+
 import java.util.ArrayList;
 import java.util.HashMap;
 
-public class GestorAcademico {
+public class GestorAcademico  implements ServiciosAcademicos {
     private ArrayList<Estudiante> estudiantes;
     private ArrayList<Curso> cursos;
     private HashMap<Curso, ArrayList<Estudiante>> inscripciones;
@@ -14,45 +16,73 @@ public class GestorAcademico {
         inscripciones = new HashMap<>();
     }
 
-    public void agregarEstudiante(Estudiante estudiante) {
-        estudiantes.add(estudiante);
-    }
-
-    public ArrayList<Estudiante> getEstudiantes() {
-        return estudiantes;
-    }
-
-    //* Cursos
-    public void agregarCurso(Curso curso) {
-        cursos.add(curso);
-    }
-
-    public ArrayList<Curso> getCursos() {
-        return cursos;
-    }
-
-    //* Estudiantes inscritos
-
-    public void agregarEstudiates(Estudiante estudiante) {
-        estudiantes.add(estudiante);
-    }
-
-    public void agregarCursos(Curso curso) {
-        cursos.add(curso);
-    }
-
-    public void inscribirEstudiante(Estudiante estudiante, Curso curso) {
-        if (inscripciones.containsKey(curso)) {
-            inscripciones.get(curso).add(estudiante);
+    @Override
+    public void matricularEstudiante(Estudiante estudiante) {
+        for (Estudiante e : estudiantes) {
+            if (e.getId() == estudiante.getId()) {
+                // Estudiante ya está matriculado
+                return;
+            }
         }
+        estudiantes.add(estudiante);
     }
 
-    public ArrayList<Estudiante> getEstudiantesInscritos(Curso curso) {
-        return inscripciones.get(curso);
+    @Override
+    public void agregarCurso(Curso curso) {
+        for (Curso c : cursos) {
+            if (c.getIdCurso() == curso.getIdCurso()) {
+                return;
+            }
+        }
+        cursos.add(curso);
+        inscripciones.put(curso, new ArrayList<Estudiante>());
+    }
+
+    @Override
+    public void inscribirEstudiante(Estudiante estudiante, int idCurso) throws EstudianteYaInscritoException {
+        Curso curso = encontrarCursoPorId(idCurso);
+        if (curso == null) {
+            throw new IllegalArgumentException("Curso no encontrado");
+        }
+
+        ArrayList<Estudiante> inscritos = inscripciones.get(curso);
+        if (inscritos.contains(estudiante)) {
+            throw new EstudianteYaInscritoException("El estudiante ya está inscrito en este curso");
+        }
+        inscritos.add(estudiante);
+    }
+
+    @Override
+    public void desinscribirEstudiante(int idEstudiante, int idCurso) throws EstudianteNoInscritoEnCursoException {
+        Curso curso = encontrarCursoPorId(idCurso);
+        if (curso == null) {
+            throw new IllegalArgumentException("Curso no encontrado");
+        }
+
+        ArrayList<Estudiante> inscritos = inscripciones.get(curso);
+        Estudiante estudiante = encontrarEstudiantePorId(idEstudiante);
+        if (estudiante == null || !inscritos.contains(estudiante)) {
+            throw new EstudianteNoInscritoEnCursoException("El estudiante no está inscrito en este curso");
+        }
+        inscritos.remove(estudiante);
     }
 
 
+    private Curso encontrarCursoPorId(int idCurso) {
+        for (Curso c : cursos) {
+            if (c.getIdCurso() == idCurso) {
+                return c;
+            }
+        }
+        return null;
+    }
 
-
-
+    private Estudiante encontrarEstudiantePorId(int id) {
+        for (Estudiante e : estudiantes) {
+            if (Integer.parseInt(e.getId()) == id){
+                return e;
+            }
+        }
+        return null;
+    }
 }
